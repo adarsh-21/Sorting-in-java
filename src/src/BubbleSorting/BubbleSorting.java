422b392d8186f1506b29bb51b3fa3c52daf44edd
Adarsh Chauhan <55660223+adarsh-21@users.noreply.github.com>
2020-04-03 19:50:04 +0100

    Update BubbleSorting.java

--- src/src/BubbleSorting/BubbleSorting.java
+++ src/src/BubbleSorting/BubbleSorting.java
@@ -28,7 +28,6 @@ public class BubbleSorting<result> {
         }
         BubbleSorting b = new BubbleSorting();
         int[] result = b.BubbleSort(arr);
-        //System.out.println(result);
         for (int i = 0; i <result.length ; i++) {
             System.out.println(result[i]);
         }
